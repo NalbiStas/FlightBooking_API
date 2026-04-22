966cd7f7f9bcbed4732e9a283de2bc80f09ceb4b
Nalbi Stas <stas.nalbi@gmail.com>
2026-04-22 13:34:00 +0200

    Added @ConfigurationPropertiesScan to enable scanning of configuration properties classes.

--- src/main/java/se/lexicon/flightbooking_api/FlightBookingApiApplication.java
+++ src/main/java/se/lexicon/flightbooking_api/FlightBookingApiApplication.java
@@ -2,8 +2,10 @@ package se.lexicon.flightbooking_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class FlightBookingApiApplication {
 
     public static void main(String[] args) {
