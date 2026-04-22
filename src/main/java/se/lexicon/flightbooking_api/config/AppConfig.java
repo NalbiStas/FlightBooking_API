db79db152230d6593068e67e82edbe4a9dce8972
Nalbi Stas <stas.nalbi@gmail.com>
2026-04-22 13:33:45 +0200

    Configured ObjectMapper bean with JavaTimeModule for better date/time handling.

--- src/main/java/se/lexicon/flightbooking_api/config/AppConfig.java
+++ src/main/java/se/lexicon/flightbooking_api/config/AppConfig.java
@@ -1,10 +1,17 @@
 package se.lexicon.flightbooking_api.config;
 
-import org.springframework.context.annotation.ComponentScan;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("se.lexicon.*")
 public class AppConfig {
 
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        return mapper;
+    }
 }
